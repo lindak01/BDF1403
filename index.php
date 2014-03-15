01cf2e209c9eb5e5d9851026b821296c52de5366
Linda <lindak01@fullsail.edu>
2014-03-15 15:49:21 -0500

    Added index.php page
    Added basic index page with connection to mysql database

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Cooking Lao</title>
+</head>
+
+<body>
+    <?php
+        mysql_connect('localhost:8888', 'root', 'root') or
+        die(mysql_error());
+        mysql_select_db('BDF1403') or die(mysql_error());
+    
+    ?>
+
+</body>
+</html>
